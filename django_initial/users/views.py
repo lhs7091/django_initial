9ce12761ad59614bf925a3451241f53cdccb84c8
lhs7091 <lhs7091@naver.com>
2020-05-05 13:13:03 +0900

    Django Project
    save function modify

--- django_initial/users/views.py
+++ django_initial/users/views.py
@@ -3,12 +3,13 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
-# Create your views here.
+
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save() # DB save, you can check admin page
+            # DB save, you can check admin page
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
