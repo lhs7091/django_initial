d78f60059c170ead39c028f33871938543052184
lhs7091 <lhs7091@naver.com>
2020-05-06 16:36:20 +0900

    Django Project
    Password reset with gmail authentication

--- django_initial/django_initial/settings.py
+++ django_initial/django_initial/settings.py
@@ -129,3 +129,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = 'lhs7091@gmail.com'
+EMAIL_HOST_PASSWORD = 'lhs1141#$'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- django_initial/users/forms.py
+++ django_initial/users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
+from django.core.exceptions import ValidationError
+
 from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
@@ -23,3 +25,12 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
+
+
+class EmailValidationOnForgotPassword(PasswordResetForm):
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not User.objects.filter(email__iexact=email, is_active=True).exists():
+            raise ValidationError("There is no user registered with the specified email address!")
+
+        return email
